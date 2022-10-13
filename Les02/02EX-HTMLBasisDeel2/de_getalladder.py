b6b3595fc9e6b52671524c582e56ad284fc48485
Esosa Izekor <114697608+esosaizekor@users.noreply.github.com>
2022-10-13 14:13:46 +0200

    Add files via upload

---
+++ Les02/02EX-HTMLBasisDeel2/de_getalladder.py
@@ -0,0 +1,3 @@
+# https://dodona.ugent.be/nl/courses/1523/series/16785/activities/257342570
+
+
---
+++ Les02/de_getalladder.py
@@ -0,0 +1,12 @@
+# https://dodona.ugent.be/nl/courses/1523/series/16785/activities/257342570
+
+
+getal = int(input())
+
+i = 0
+
+while i < getal:
+    i+=1  
+    print()
+    for indexa in range(1,i+1):
+        print(indexa, end="")
