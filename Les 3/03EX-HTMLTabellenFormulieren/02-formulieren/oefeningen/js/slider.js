ac093a9631ddf5fce9f6abd918e1dcef4b293674
Esosa Izekor <114697608+esosaizekor@users.noreply.github.com>
2022-10-13 14:06:45 +0200

    Delete Les 3 directory

--- Les 3/03EX-HTMLTabellenFormulieren/02-formulieren/oefeningen/js/slider.js
+++
@@ -1,6 +0,0 @@
-const slider = document.getElementById("score");
-const score = document.getElementById("showScore");
-slider.addEventListener("input", () => {
-  score.innerText = slider.value;
-});
-score.innerText = slider.value;
